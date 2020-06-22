20bcfbd8e1097e6047fcd4ea199c26eb8b27d1e4
Freddy silva <silvahfreddy@gmail.com>
2020-06-22 17:56:05 -0500

    se agrego imagenes

--- resources/views/envasados.blade.php
+++ resources/views/envasados.blade.php
@@ -3,16 +3,16 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
+        <div class="col-12">
             <div class="card">
                 <div class="card-header">
                     <div class="row">
-                        <div class="col-10">
+                        <div class="col-sm-9 col-md-10 col-lg-10">
                             <h2>
                                 {{ $plantas->nombre }} Listado de contadores
                             </h2>
                         </div>
-                        <div class="col-2">
+                        <div class="col-sm-3 col-md-2 col-lg-2">
                             <a class="btn btn-outline-primary" href="{{ route('home') }}">Regresar</a>
                         </div>
                     </div>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,27 +1,25 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
+<div class="container-fluid">
     <div class="row justify-content-center">
         <div class="col">
-
-<h1>Plantas a observar</h1>
-<div class="row">
-    @foreach ($plantas as $item)
-        <div class="col-3 m-2">
-            <div class="card" style="width: 18rem;">
-                <figure class="w-100">
-                    <img src="{{ asset('img/planta-backus.jpg')}}" class="card-img-top" alt="logo">
-                </figure>
-                <div class="card-body">
-                <h5 class="card-title">{{ $item->nombre }}</h5>
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the cards content.</p>
-                <a href="{{ route('envasados',$item->id) }}" class="btn btn-primary btn-block">Ver</a>
-                </div>
+            <div class="row">
+                @foreach ($plantas as $item)
+                    <div class="col-sm-12 col-md-4 col-lg-3 mb-2">
+                        <div class="card">
+                            <figure class="w-100">
+                                <img src="{{ asset('img/planta-backus.jpg')}}" class="card-img-top" alt="logo">
+                            </figure>
+                            <div class="card-body">
+                            <h5 class="card-title text-primary text-uppercase">{{ $item->nombre }}</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the cards content.</p>
+                            <a href="{{ route('envasados',$item->id) }}" class="btn btn-primary btn-block">Click para ver</a>
+                            </div>
+                        </div>
+                    </div>
+                @endforeach
             </div>
-        </div>
-    @endforeach
-</div>
         </div>
     </div>
 </div>
